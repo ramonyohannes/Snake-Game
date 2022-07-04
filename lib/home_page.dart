54f5f383fc8618a7e2c2cdeb42d57068a62d2aad
ramon ebeyan <ramonebeyan@gmail.com>
2022-07-04 15:11:40 +0300

    Added Gesture detector for Left, Right,Top and Bottom for the user to tap to make the snake move on tapped direction

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,8 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+enum SnakeDirection { UP, DOWN, LEFT, RIGHT }
+
 class _HomePageState extends State<HomePage> {
   //Grid Dimenstions
   int rowSize = 10;
@@ -24,16 +26,57 @@ class _HomePageState extends State<HomePage> {
   //Food Pos
   int foodPos = 55;
 
+  //Snake Direction
+  var snakeDirection = SnakeDirection.RIGHT;
+
   //Start Game
   void startGame() {
-    Timer.periodic(Duration(milliseconds: 200), (timer) {
+    Timer.periodic(const Duration(milliseconds: 200), (timer) {
       setState(() {
-        //add new head
-        snakePos.add(snakePos.last + 1);
-        //remove the tail
-        snakePos.removeAt(0);
+        snake();
       });
     });
+  }
+
+  void snake() {
+    switch (snakeDirection) {
+      case SnakeDirection.RIGHT:
+        {
+          //add new head
+          snakePos.add(snakePos.last + 1);
+          //remove the tail
+          snakePos.removeAt(0);
+        }
+        break;
+
+      case SnakeDirection.LEFT:
+        {
+          //add new head
+          snakePos.add(snakePos.last - 1);
+          //remove the tail
+          snakePos.removeAt(0);
+        }
+        break;
+
+      case SnakeDirection.DOWN:
+        {
+          //add new head
+          snakePos.add(snakePos.last + rowSize);
+          //remove the tail
+          snakePos.removeAt(0);
+        }
+        break;
+
+      case SnakeDirection.UP:
+        {
+          //add new head
+          snakePos.add(snakePos.last - rowSize);
+          //remove the tail
+          snakePos.removeAt(0);
+        }
+        break;
+      default:
+    }
   }
 
   @override
@@ -49,21 +92,49 @@ class _HomePageState extends State<HomePage> {
           //Game space
           Expanded(
             flex: 3,
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: rowSize,
-              ),
-              itemCount: totalNumberOfSquares,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                if (snakePos.contains(index)) {
-                  return const SnakePixel();
-                } else if (foodPos == index) {
-                  return const FoodPixel();
-                } else {
-                  return BlankPixel();
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.delta.dy > 0 &&
+                    snakeDirection != SnakeDirection.UP) {
+                  setState(() {
+                    snakeDirection = SnakeDirection.DOWN;
+                  });
+                } else if (details.delta.dy < 0 &&
+                    snakeDirection != SnakeDirection.DOWN) {
+                  setState(() {
+                    snakeDirection = SnakeDirection.UP;
+                  });
                 }
               },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0 &&
+                    snakeDirection != SnakeDirection.LEFT) {
+                  setState(() {
+                    snakeDirection = SnakeDirection.RIGHT;
+                  });
+                } else if (details.delta.dx < 0 &&
+                    snakeDirection != SnakeDirection.RIGHT) {
+                  setState(() {
+                    snakeDirection = SnakeDirection.LEFT;
+                  });
+                }
+              },
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: rowSize,
+                ),
+                itemCount: totalNumberOfSquares,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  if (snakePos.contains(index)) {
+                    return const SnakePixel();
+                  } else if (foodPos == index) {
+                    return const FoodPixel();
+                  } else {
+                    return BlankPixel();
+                  }
+                },
+              ),
             ),
           ),
           //Playbutton
